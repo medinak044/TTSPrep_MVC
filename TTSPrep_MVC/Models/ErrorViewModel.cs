8c331c1e800f8982cb82b2c9a6b8d179ba05427d
medinak044 <63528703+medinak044@users.noreply.github.com>
2023-12-22 12:23:34 -0600

    Initial commit

---
+++ TTSPrep_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TTSPrep_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
